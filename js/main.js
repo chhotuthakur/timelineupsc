b06403ae946e8a04b881ad123b93a5735837a652
chhotuthakur <npsdahiya@gmail.com>
2023-05-19 11:28:19 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var config = {
+
+    apiKey: "AIzaSyD-iVW626fXLl6Itv2D8VTHek3HW7dyjdk",
+    authDomain: "upscqapp.firebaseapp.com",
+    databaseURL: "https://upscqapp.firebaseio.com",
+    projectId: "upscqapp",
+    storageBucket: "upscqapp.appspot.com",
+    messagingSenderId: "623746098558",
+    appId: "1:623746098558:web:0eb079b41e3231c5d27f2f",
+    measurementId: "G-SC6H3B2D2H"
+   
+   
+      
+     };
+     firebase.initializeApp(config);
+   
+
+  // Initialize Firebase
+//   const app = initializeApp(firebaseConfig);
+//   const analytics = getAnalytics(app);
